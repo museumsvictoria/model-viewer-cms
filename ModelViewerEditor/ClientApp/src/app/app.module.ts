57d2bd129aa365b0ab06d851a7832c1339a65f3b
Forbes Hawkins <fhawkins@museum.vic.gov.au>
2021-06-22 13:11:00 +1000

    Import material modules

--- ModelViewerEditor/ClientApp/src/app/app.module.ts
+++ ModelViewerEditor/ClientApp/src/app/app.module.ts
@@ -9,6 +9,16 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatCardModule } from "@angular/material/card";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 import { NewProjectComponent } from './new-project/new-project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ViewProjectComponent } from './view-project/view-project.component';
@@ -27,7 +37,17 @@ import { ViewProjectComponent } from './view-project/view-project.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    FlexLayoutModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSnackBarModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
