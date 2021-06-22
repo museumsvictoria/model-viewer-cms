bb185a825a1b11d07be901092a6178eb9b120517
Forbes Hawkins <fhawkins@museum.vic.gov.au>
2021-06-22 13:04:07 +1000

    Add ng-scaffold project components

--- ModelViewerEditor/ClientApp/src/app/app.module.ts
+++ ModelViewerEditor/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ViewProjectComponent } from './view-project/view-project.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    NewProjectComponent,
+    ProjectListComponent,
+    ViewProjectComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
