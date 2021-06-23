9d00a34b1ef460b38cad370a89442aee0b1827bf
Forbes Hawkins <fhawkins@museum.vic.gov.au>
2021-06-23 11:34:25 +1000

    Misc code tidy

--- ModelViewerEditor/ClientApp/src/app/new-project-dialog/new-project-dialog.component.ts
+++ ModelViewerEditor/ClientApp/src/app/new-project-dialog/new-project-dialog.component.ts
@@ -1,50 +1,52 @@
-import {Component, EventEmitter, Inject, OnInit, Output} from "@angular/core";
+import { Component, EventEmitter, Inject, OnInit, Output } from "@angular/core";
 import { ProjectModel } from "../shared/models/projectModel";
 import { DataService } from "../shared/services/data.service";
 import { Router } from "@angular/router";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {AbstractControl, FormControl, ValidationErrors, Validators} from "@angular/forms";
-import {debounceTime, map} from "rxjs/operators";
-import {Observable} from "rxjs";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import {
+  AbstractControl,
+  FormControl,
+  ValidationErrors,
+  Validators,
+} from "@angular/forms";
+import { debounceTime, first, map } from "rxjs/operators";
+import { Observable, pipe } from "rxjs";
 
 @Component({
   templateUrl: "./new-project-dialog.component.html",
   styleUrls: ["./new-project-dialog.component.scss"],
 })
 export class NewProjectDialogComponent implements OnInit {
-
-
-  projectName = new FormControl('', { validators: [
-      Validators.required
-    ], asyncValidators: [this.validateNameViaServer.bind(this)]  });
-
+  projectName = new FormControl("", {
+    validators: [Validators.required],
+    asyncValidators: [this.validateNameViaServer.bind(this)],
+  });
 
   constructor(
     private dataService: DataService,
     private dialogRef: MatDialogRef<NewProjectDialogComponent>
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {}
 
-
   onSubmit_click() {
-    let r = this.dataService.addProject(this.projectName.value);
-    if (r.success) {
-      this.dialogRef.close();
-    }
+    console.log("calling dataservice");
+    this.dataService.addProject(this.projectName.value).subscribe(
+      () => this.dialogRef.close(),
+      (err) => console.log(err)
+    );
   }
 
   onCancel_click() {
     this.dialogRef.close();
   }
-  validateNameViaServer(ctrl: AbstractControl) : Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-    return this.dataService.projectExists(ctrl.value).pipe(
-      map(x => (x ? { nameExists: true } : null))
-    );
+
+  validateNameViaServer(
+    ctrl: AbstractControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return this.dataService
+      .projectExists(ctrl.value)
+      .pipe(map((x) => (x ? { nameExists: true } : null)));
   }
-
 }
-
-
