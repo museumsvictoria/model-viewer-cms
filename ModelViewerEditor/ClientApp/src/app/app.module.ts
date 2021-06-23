4e0da2e964389834d22c9f285a9a0f94fd4171d5
Forbes Hawkins <fhawkins@museum.vic.gov.au>
2021-06-23 11:31:21 +1000

    Extract routes to module

--- ModelViewerEditor/ClientApp/src/app/app.module.ts
+++ ModelViewerEditor/ClientApp/src/app/app.module.ts
@@ -1,13 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { CounterComponent } from "./counter/counter.component";
+import { FetchDataComponent } from "./fetch-data/fetch-data.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
@@ -17,11 +15,12 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatCardModule } from "@angular/material/card";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
-import { ProjectListComponent } from './project-list/project-list.component';
-import { ViewProjectComponent } from './view-project/view-project.component';
-import {MatDialogModule} from "@angular/material/dialog";
-import {NewProjectDialogComponent} from "./new-project-dialog/new-project-dialog.component";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { ProjectListComponent } from "./project-list/project-list.component";
+import { ViewProjectComponent } from "./view-project/view-project.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { NewProjectDialogComponent } from "./new-project-dialog/new-project-dialog.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -31,10 +30,10 @@ import {NewProjectDialogComponent} from "./new-project-dialog/new-project-dialog
     FetchDataComponent,
     NewProjectDialogComponent,
     ProjectListComponent,
-    ViewProjectComponent
+    ViewProjectComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
+    BrowserModule,
     HttpClientModule,
     FlexLayoutModule,
     FormsModule,
@@ -48,13 +47,10 @@ import {NewProjectDialogComponent} from "./new-project-dialog/new-project-dialog
     MatInputModule,
     MatCardModule,
     MatSnackBarModule,
-    RouterModule.forRoot([
-      {path: '', component: HomeComponent, pathMatch: 'full'},
-      {path: 'fetch-data', component: FetchDataComponent},
-    ]),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
