bc24633363997ba546476c1d6a042eefd5a14bba
Forbes Hawkins <fhawkins@museum.vic.gov.au>
2021-06-24 09:24:06 +1000

    misc clean

--- ModelViewerEditor/ClientApp/src/app/home/home.component.ts
+++ ModelViewerEditor/ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
 import { DataService } from "../shared/services/data.service";
 import { ProjectModel } from "../shared/models/projectModel";
-import {MatDialog} from "@angular/material/dialog";
-import {NewProjectDialogComponent} from "../new-project-dialog/new-project-dialog.component";
-import {first} from "rxjs/operators";
+import { MatDialog } from "@angular/material/dialog";
+import { NewProjectDialogComponent } from "../new-project-dialog/new-project-dialog.component";
+import { first } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
@@ -11,27 +11,23 @@ import {first} from "rxjs/operators";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
-  constructor(private dataService: DataService,private dialog: MatDialog) {
-    this.dataService.getProjects().pipe(first()).subscribe(x => this.projects = x);
+  constructor(private dataService: DataService, private dialog: MatDialog) {
+    this.dataService
+      .getProjects()
+      .pipe(first())
+      .subscribe((x) => (this.projects = x));
   }
 
   projects: ProjectModel[];
 
-  selectedProject: ProjectModel;
-
   onNewProject_click() {
     const dialogRef = this.dialog.open(NewProjectDialogComponent, {
-      height: '400px',
-      width: '600px',
+      height: "400px",
+      width: "600px",
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-    //  this.dataService.getProjects().subscribe(x => this.projects = x);
-    })
-
-
-
+    dialogRef.afterClosed().subscribe((result) => {
+      //  this.dataService.getProjects().subscribe(x => this.projects = x);
+    });
   }
-
-
 }
