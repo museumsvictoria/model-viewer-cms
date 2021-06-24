0fdbdfd6af32f2c102850fa9b1267615f775bb38
Forbes Hawkins <fhawkins@museum.vic.gov.au>
2021-06-24 10:59:47 +1000

    Fill out new-model-dialog.component

--- ModelViewerEditor/ClientApp/src/app/new-model-dialog/new-model-dialog.component.ts
+++ ModelViewerEditor/ClientApp/src/app/new-model-dialog/new-model-dialog.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from "@angular/core";
+import {
+  AbstractControl,
+  FormControl,
+  ValidationErrors,
+  Validators,
+} from "@angular/forms";
+import { Observable, of, throwError } from "rxjs";
+import { map, switchMap } from "rxjs/operators";
+import { DataService } from "../shared/services/data.service";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import {ProjectModel, SectionModel} from "../shared/models/projectModel";
 
 @Component({
-  selector: 'app-new-model-dialog',
-  templateUrl: './new-model-dialog.component.html',
-  styleUrls: ['./new-model-dialog.component.scss']
+  selector: "app-new-model-dialog",
+  templateUrl: "./new-model-dialog.component.html",
+  styleUrls: ["./new-model-dialog.component.scss"],
 })
 export class NewModelDialogComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: { section: SectionModel },
+    private dataService: DataService,
+    private dialogRef: MatDialogRef<NewModelDialogComponent>
+  ) {}
 
-  constructor() { }
+  modelName = new FormControl("", {
+    validators: [Validators.required],
+    asyncValidators: [this.validateNameViaServer.bind(this)],
+  });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit_click() {
+    this.dataService
+      .addModel(this.data.section, this.modelName.value)
+      .subscribe(
+        () => {
+          this.dialogRef.close();
+        },
+        (err) => console.log(err)
+      );
   }
 
+  onCancel_click() {
+    this.dialogRef.close();
+  }
+
+  validateNameViaServer(
+    ctrl: AbstractControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    var r = this.data.section.objects.some(
+      (x) => x.name.trim().toLowerCase() == ctrl.value.trim().toLowerCase()
+    );
+    return of(r ? { nameExists: true } : null);
+  }
 }
